72b0da8dd5ebc3129dd63602b7b8402d44f2d7ed
Вячеслав Андрющенко <31081994@mail.ru>
2022-07-21 17:03:15 +0700

    Ready

---
+++ files1.1.py
@@ -0,0 +1,29 @@
+with open('cook_book.txt') as file:
+    cook_book = {}
+    for line in file:
+        dish = line.strip()
+        ingredient_list = []
+        for item in range(int(file.readline())):
+            ingredient_dict = {}
+            name, quantity, measure = file.readline().split(" |")
+            ingredient_dict['ingredient_name'] = name.strip(' \n')
+            ingredient_dict['quantity'] = int(quantity.strip(' \n'))
+            ingredient_dict['measure'] = measure.strip(' \n')
+            ingredient_list.append(ingredient_dict)
+        cook_book[dish] = ingredient_list
+        file.readline()
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for items in dishes:
+        for keys, values in cook_book.items():
+            if keys in items:
+                for value in values:
+                    value['quantity'] *= person_count
+                    shop_list[value.pop('ingredient_name')] = value
+    
+    return print(shop_list)
+
+# print(cook_book)
+get_shop_list_by_dishes(['Омлет'], 2)
